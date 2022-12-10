483d69f29d2d73d5e271b1612ce10282f59c827a
PedroPardoG <112125266+PedroPardoG@users.noreply.github.com>
2022-12-09 19:51:23 -0600

    nueva tabkla solicitudes

--- controllers/solicitudes y comentarios/createSolicitud.js
+++ controllers/solicitudes y comentarios/createSolicitud.js
@@ -5,20 +5,74 @@ const db = require("../../config/db.js");
 
 module.exports = {
     createSolicitud: (req, res) => {
-        const IdUsuario = req.body.IdUsuario;
-        const DatosAdicionales = req.body.DatosAdicionales;
+        const Nombre = req.body.Nombre;
+        const APaterno = req.body.APaterno;
+        const AMaterno = req.body.AMaterno;
+        const NombreUsuario = req.body.NombreUsuario;
+        const Email = req.body.Email;
+        const Curp = req.body.Curp;
+        const RFC = req.body.RFC;
+        const Celular = req.body.Celular;
+        const Telefono = req.body.Telefono;
+        const Extencion = req.body.Extencion;
         const TipoSolicitud = req.body.TipoSolicitud;
-        const IdUsuarioCreador = req.body.CreadoPor;
+        const DatosAdicionales = req.body.DatosAdicionales;
         const IdApp = req.body.IdApp;
+        const IdUsuarioCreador = req.body.CreadoPor;
 
-        if (IdUsuario == null || /^[\s]*$/.test(IdUsuario)) {
+        if (Nombre == null || /^[\s]*$/.test(Nombre)) {
             return res.status(409).send({
-                error: "Ingrese ID Institucion",
+                error: "Ingrese Nombre",
+            });
+        }
+        if (APaterno == null || /^[\s]*$/.test(APaterno)) {
+            return res.status(409).send({
+                error: "Ingrese Apellido Paterno",
+            });
+        }
+        if (AMaterno == null || /^[\s]*$/.test(AMaterno)) {
+            return res.status(409).send({
+                error: "Ingrese Apellido Materno",
+            });
+        }
+        if (NombreUsuario == null || /^[\s]*$/.test(NombreUsuario)) {
+            return res.status(409).send({
+                error: "Ingrese Nombre de Usuario",
+            });
+        }
+        if (Email == null || /^[\s]*$/.test(Email)) {
+            return res.status(409).send({
+                error: "Ingrese Email",
+            });
+        }
+        if (Curp == null || /^[\s]*$/.test(Curp)) {
+            return res.status(409).send({
+                error: "Ingrese Curp",
+            });
+        }
+        if (RFC == null || /^[\s]*$/.test(RFC)) {
+            return res.status(409).send({
+                error: "Ingrese RFC",
+            });
+        }
+        if (Celular == null || /^[\s]*$/.test(Celular)) {
+            return res.status(409).send({
+                error: "Ingrese Celular",
+            });
+        }
+        if (Telefono == null || /^[\s]*$/.test(Telefono)) {
+            return res.status(409).send({
+                error: "Ingrese Telefono",
+            });
+        }
+        if (Extencion == null || /^[\s]*$/.test(Extencion)) {
+            return res.status(409).send({
+                error: "Ingrese Extencion del Telefono",
             });
         }
         if (IdUsuarioCreador == null || /^[\s]*$/.test(IdUsuarioCreador)) {
             return res.status(409).send({
-                error: "Ingrese ID Institucion",
+                error: "Ingrese ID Usaurio Creador",
             });
         }
 
@@ -27,13 +81,21 @@ module.exports = {
                 error: "Ingrese tipo de solicitud",
             });
         } 
+
+
+        if (DatosAdicionales == null ) {
+            return res.status(409).send({
+                error: "Ingrese Id Datos adicionales ",
+            });
+        }
+        
         if (IdApp == null || /^[\s]*$/.test(IdApp)) {
             return res.status(409).send({
                 error: "Ingrese Id App",
             });
         }
 
-                db.query(`CALL sp_CreaSolicitud('${IdUsuario}', '${DatosAdicionales}', '${TipoSolicitud}', '${IdUsuarioCreador}', '${IdApp}')`, (err, result) => {
+                db.query(`CALL sp_CreaSolicitud('${Nombre}','${APaterno}', '${AMaterno}', '${NombreUsuario}', '${Email}', '${Curp}', '${RFC}', '${Celular}', '${Telefono}', '${Extencion}', '${TipoSolicitud}', '${DatosAdicionales}', '${IdApp}','${IdUsuarioCreador}')`, (err, result) => {
                 if (err) {
                     return res.status(500).send({
                         error: "Error",
