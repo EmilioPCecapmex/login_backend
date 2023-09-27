0a73f5858f72a75fb6bb283e5243f80e6cc741cf
EdithLizcano <126712189+EdithLizcano@users.noreply.github.com>
2023-09-27 16:10:33 -0600

    ayudaadm

---
+++ controllers/ayudas/Ayudas.js
@@ -0,0 +1,36 @@
+const db = require("../../config/db.js");
+module.exports = {
+
+AdminAyudas: (req, res) => {
+ const {NUMOPERACION} = req.body;
+
+ if (NUMOPERACION == 1){
+    const {VIDEO} = req.body;
+ }
+},
+
+AdminVideoTutoriales: (req, res) => {
+
+},
+
+AdminPreguntasFrecuentes: (req, res) => {
+
+},
+
+AdminGuiaRapida: (req, res) => {
+
+},
+
+obtenerguias: (req, res) => {
+
+},
+
+obtenerDoc: (req, res) => {
+
+},
+
+SelectIndex: (req, res) => {
+
+},
+
+}
