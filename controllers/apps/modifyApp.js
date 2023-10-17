4c22c2696906c416020dc0982f656f38667a28c4
PedroPardoG <prpardo@cecapmex.com>
2023-10-17 15:58:19 -0600

    desactivar aplicaciones

--- controllers/apps/modifyApp.js
+++ controllers/apps/modifyApp.js
@@ -8,19 +8,24 @@ module.exports = {
     const path = req.body.Path;
     const userUpdateId = req.body.IdUsuarioModificador;
     const estaActivo = req.body.EstaActivo;
-    const query = 'UPDATE `Apps` '+
-    'SET `Nombre` = ?, `Descripcion` = ? , `Path` = ? , `EstaActivo` = ? , `ModificadoPor` = ? ' +
-    'WHERE `id` = ?';
-    const values = [name, des, path,estaActivo,userUpdateId,appId];
+    // const query = 'UPDATE `Apps` '+
+    // 'SET `Nombre` = ?, `Descripcion` = ? , `Path` = ? , `EstaActivo` = ? , `ModificadoPor` = ? ' +
+    // 'WHERE `id` = ?';
+    // const values = [name, des, path,estaActivo,userUpdateId,appId];
+    const query=`CALL sp_ModificaApp(?,?,?,?,?,?)`
+    const values = [appId,name,path,userUpdateId,des,estaActivo];
 
+    console.log("values",values);
     db.query(query, values, (err, result) => {
+      console.log("err",err);
+      console.log("result",result[0][0].Mensaje);
           if (err) {
               return res.status(500).send({
-                error: "Error",
+                error: err,
               });
             }
-            if (result.affectedRows!=0) {
-              const data = result.affectedRows;
+            if (result[0][0].Mensaje==="Ok") {
+              data=result[0][0]
               if (data === undefined) {
                 return res.status(409).send({
                   error: "¡Sin Información!",
