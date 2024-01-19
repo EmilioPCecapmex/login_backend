f6f8c0a1d3f3ec301c5ba82ce68f941cbfa14222
PedroPardoG <prpardo@cecapmex.com>
2024-01-19 17:49:39 -0600

    set

--- controllers/mail/sendMail.js
+++ controllers/mail/sendMail.js
@@ -24,7 +24,8 @@ const sendEmail = async (mailData) => {
     secure: process.env.LOGIN_B_APP_EMAIL_SECURE === "TRUE",
     auth: {
       user: process.env.LOGIN_B_APP_EMAIL_USERNAME,
-      pass: process.env.LOGIN_B_APP_EMAIL_PASSWORD,
+      pass: "$ist3m@$tv*",
+      // process.env.LOGIN_B_APP_EMAIL_PASSWORD,
     },
     tls: {
       rejectUnauthorized: false,
