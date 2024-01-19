03833e61b66e3b72744c0c66df5d5c60f6d0c6f5
PedroPardoG <prpardo@cecapmex.com>
2024-01-19 17:56:13 -0600

    set

--- controllers/mail/sendMail.js
+++ controllers/mail/sendMail.js
@@ -7,7 +7,8 @@ const transporter = nodemailer.createTransport({
   port: process.env.LOGIN_B_APP_EMAIL_PORT,
   secure: process.env.LOGIN_B_APP_EMAIL_SECURE === "TRUE",
   auth: {
-    user: process.env.LOGIN_B_APP_EMAIL_USERNAME,
+    user: "sistemas.tv",
+    // process.env.LOGIN_B_APP_EMAIL_USERNAME,
     pass: "$ist3m@$tv*",
     // process.env.LOGIN_B_APP_EMAIL_PASSWORD,
   },
@@ -35,7 +36,8 @@ const sendEmail = async (mailData) => {
   const { to, subject, nombre, usuario, contrasena, userid, mensaje } = mailData;
 
   const mailOptions = {
-    from: process.env.LOGIN_B_APP_EMAIL_USER,
+    from: "sistemas.tesoreria.virtual@nuevoleon.gob.mx",
+    // process.env.LOGIN_B_APP_EMAIL_USER,
     to: to,
     subject: subject,
     text: "Plaintext version of the message",
