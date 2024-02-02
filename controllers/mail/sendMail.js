9274b223accb237051bdc5388785ef5ad2195a2b
PedroPardoG <prpardo@cecapmex.com>
2024-02-02 12:11:48 -0600

    send mail

--- controllers/mail/sendMail.js
+++ controllers/mail/sendMail.js
@@ -3,13 +3,12 @@ const { emailTemplate } = require("../mail/newUser");
 const util = require('util');
 
 const transporter = nodemailer.createTransport({
-  host: 'correo.nl.gob.mx',
-  port: 587,
-  secure: false,
-  // process.env.LOGIN_B_APP_EMAIL_SECURE === "TRUE",
+  host: process.env.LOGIN_B_APP_EMAIL_HOST,
+  port: process.env.LOGIN_B_APP_EMAIL_PORT,
+  secure: process.env.LOGIN_B_APP_EMAIL_SECURE === "TRUE",
   auth: {
-    user: "sistemas.tv",
-    pass: "$ist3m@$tv*",
+    user: process.env.LOGIN_B_APP_EMAIL_USERNAME,
+    pass: process.env.LOGIN_B_APP_EMAIL_PASSWORD,
   },
   tls: {
     rejectUnauthorized: false,
@@ -20,9 +19,9 @@ const sendMailPromise = util.promisify(transporter.sendMail).bind(transporter);
 
 const sendEmail = async (mailData) => {
   const { to, subject, nombre, usuario, contrasena, userid, mensaje } = mailData;
- 
+
   const mailOptions = {
-    from: "sistemas.tesoreria.virtual@nuevoleon.gob.mx",
+    from: process.env.LOGIN_B_APP_EMAIL_USER,
     to: to,
     subject: subject,
     text: "Plaintext version of the message",
@@ -40,3 +39,4 @@ const sendEmail = async (mailData) => {
 module.exports = {
   sendEmail: sendEmail,
 };
+
