f26b32924affbce4ab7cf115400034a783e3be9e
PedroPardoG <112125266+PedroPardoG@users.noreply.github.com>
2024-04-18 16:05:45 -0600

    log email

--- controllers/mail/sendMail.js
+++ controllers/mail/sendMail.js
@@ -4,17 +4,17 @@ const util = require('util');
 const { emailVinculacionTemplate } = require("./confirmacionVinculacion");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.LOGIN_B_APP_EMAIL_HOST,
-  // 'correo.nl.gob.mx',
-  port: process.env.LOGIN_B_APP_EMAIL_PORT,
-  // 587,
-  secure:  process.env.LOGIN_B_APP_EMAIL_SECURE === "TRUE",
-  //false,
+  host: // process.env.LOGIN_B_APP_EMAIL_HOST,
+  'correo.nl.gob.mx',
+  port: // process.env.LOGIN_B_APP_EMAIL_PORT,
+  587,
+  secure:  //process.env.LOGIN_B_APP_EMAIL_SECURE === "TRUE",
+  false,
   auth: {
-    // user: "sistemas.tv",
-    // pass: "$ist3m@$tv*",
-    user: process.env.LOGIN_B_APP_EMAIL_USERNAME,
-    pass: process.env.LOGIN_B_APP_EMAIL_PASSWORD,
+     user: "sistemas.tv",
+     pass: "$ist3m@$tv*",
+    //user: process.env.LOGIN_B_APP_EMAIL_USERNAME,
+    //pass: process.env.LOGIN_B_APP_EMAIL_PASSWORD,
   },
   tls: {
     rejectUnauthorized: false,
@@ -27,14 +27,14 @@ const sendEmail = async (mailData) => {
   const { to, subject, nombre, usuario, contrasena, userid, mensaje } = mailData;
  
   const mailOptions = {
-    from: process.env.LOGIN_B_APP_EMAIL_USER,
-    // "sistemas.tesoreria.virtual@nuevoleon.gob.mx",
+    from://  process.env.LOGIN_B_APP_EMAIL_USER,
+    "sistemas.tesoreria.virtual@nuevoleon.gob.mx",
     to: to,
     subject: subject,
     text: "Plaintext version of the message",
     html: emailTemplate(mensaje, nombre, usuario, contrasena, userid),
   };
-
+  console.log('mailOptions',mailOptions);
   try {
     const info = await sendMailPromise(mailOptions);
     return "Correo enviado con éxito:", info.response;
@@ -47,14 +47,14 @@ const sendEmailVinculacion = async (mailData) => {
   const { to, subject, nombre, usuario, userid, mensaje } = mailData;
  
   const mailOptions = {
-    from: process.env.LOGIN_B_APP_EMAIL_USER,
-    // "sistemas.tesoreria.virtual@nuevoleon.gob.mx",
+    from: //process.env.LOGIN_B_APP_EMAIL_USER,
+     "sistemas.tesoreria.virtual@nuevoleon.gob.mx",
     to: to,
     subject: subject,
     text: "Plaintext version of the message",
     html: emailVinculacionTemplate(mensaje, nombre, usuario, userid),
   };
-
+  console.log('mailOptions',mailOptions);
   try {
     const info = await sendMailPromise(mailOptions);
     console.log("Correo enviado con éxito:");
