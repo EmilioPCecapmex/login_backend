66b38244f20615327cfcbd78e3a50fc930f21f5c
PedroPardoG <prpardo@cecapmex.com>
2024-06-26 17:11:48 -0600

    avisos generales

--- controllers/AvisosApp/AdminAvisos.js
+++ controllers/AvisosApp/AdminAvisos.js
@@ -2,73 +2,154 @@ const db = require("../../config/db.js");
 module.exports = {
 
   createAdminAvisos: (req, res) => {
-    const {IdApp,Titulo,TextoInc,IdUsuario,FechaInicio,FechaFin} = req.body;
-            let contError=0;
-            let error = "Ingrese:";
+    const { Apps, Titulo, TextoInc, IdUsuario, FechaInicio, FechaFin } = req.body;
+    let error = "Ingrese:";
+    let appsArray = JSON.parse(Apps);
+
+    // Validaciones iniciales
+    if (appsArray.length === 0) {
+        error += " Aplicaciones,";
+    }
+    if (!TextoInc || (/^[\s]*$/.test(TextoInc))) {
+        error += " Aviso,";
+    }
+    if (!IdUsuario || (/^[\s]*$/.test(IdUsuario))) {
+        error += " IdUsuario,";
+    }
+    if (!FechaInicio || (/^[\s]*$/.test(FechaInicio))) {
+        error += " Fecha Inicio,";
+    }
+    if (!FechaFin || (/^[\s]*$/.test(FechaFin))) {
+        error += " Fecha Fin,";
+    }
+    if (!Titulo || (/^[\s]*$/.test(Titulo))) {
+        error += " Titulo,";
+    }
+
+    // Si hay errores, enviar respuesta con error 400
+    if (error !== "Ingrese:") {
+        error = error.endsWith(',') ? error.slice(0, -1) : error;
+        error = error.replace(/,([^,]*)$/, ' y$1');
+        return res.status(400).send({ error });
+    }
+
+    // Iniciar la transacción
+    db.beginTransaction(err => {
+        if (err) {
+            return res.status(500).send({ error: err.message });
+        }
+
+        const queries = [];
+
+        // Construir las consultas para cada elemento en appsArray
+        appsArray.forEach(app => {
+            queries.push(new Promise((resolve, reject) => {
+                const { Id } = app; // Asumiendo que Id es necesario para cada llamada a sp_CrearAviso
+                db.query(`CALL sp_CrearAviso(?,?,?,?,?,?)`, [Id, TextoInc, IdUsuario, FechaInicio, FechaFin, Titulo, 0], (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        if (result.length) {
+                            resolve(result[0]);
+                        } else {
+                            reject(new Error("¡Sin Información!"));
+                        }
+                    }
+                });
+            }));
+        });
+
+        // Ejecutar todas las consultas en paralelo
+        Promise.all(queries)
+            .then(results => {
+                // Commit si todas las consultas fueron exitosas
+                db.commit(err => {
+                    if (err) {
+                        return db.rollback(() => {
+                            res.status(500).send({ error: err.message });
+                        });
+                    }
+                    res.status(200).send({ data: results });
+                });
+            })
+            .catch(err => {
+                // Rollback en caso de error en alguna consulta
+                db.rollback(() => {
+                    res.status(500).send({ error: err.message });
+                });
+            });
+    });
+},
+
+  // createAdminAvisos: (req, res) => {
+  //   const {Apps,Titulo,TextoInc,IdUsuario,FechaInicio,FechaFin} = req.body;
+  //           let contError=0;
+  //           let error = "Ingrese:";
+  //           let appsArray=JSON.parse(Apps)
             
     
-                if (!IdApp || (/^[\s]*$/.test(IdApp)))
-                {
-                    error += " IdApp,";
-                    contError++;
-                }
-                if (!TextoInc || (/^[\s]*$/.test(TextoInc)))
-                {
-                    error += " Aviso,";
-                    contError++;
-                } 
-                if (!IdUsuario || (/^[\s]*$/.test(IdUsuario)))
-                {
-                    error += " IdUsuario,";
-                    contError++;
-                } 
-                if (!FechaInicio || (/^[\s]*$/.test(FechaInicio)))
-                {
-                    error += " Fecha Inicio";
-                    contError++;
-                } 
-                if (!FechaFin || (/^[\s]*$/.test(FechaFin)))
-                {
-                    error += " Fecha Fin";
-                    contError++;
-                } 
-                if (!Titulo || (/^[\s]*$/.test(Titulo)))
-                  {
-                      error += " Titulo";
-                      contError++;
-                  } 
+  //               if (appsArray.length===0)
+  //               {
+  //                   error += " Aplicaciones,";
+  //                   contError++;
+  //               }
+  //               if (!TextoInc || (/^[\s]*$/.test(TextoInc)))
+  //               {
+  //                   error += " Aviso,";
+  //                   contError++;
+  //               } 
+  //               if (!IdUsuario || (/^[\s]*$/.test(IdUsuario)))
+  //               {
+  //                   error += " IdUsuario,";
+  //                   contError++;
+  //               } 
+  //               if (!FechaInicio || (/^[\s]*$/.test(FechaInicio)))
+  //               {
+  //                   error += " Fecha Inicio";
+  //                   contError++;
+  //               } 
+  //               if (!FechaFin || (/^[\s]*$/.test(FechaFin)))
+  //               {
+  //                   error += " Fecha Fin";
+  //                   contError++;
+  //               } 
+  //               if (!Titulo || (/^[\s]*$/.test(Titulo)))
+  //                 {
+  //                     error += " Titulo";
+  //                     contError++;
+  //                 } 
             
-                // Elimina la última coma si existe
-                error = error.endsWith(',') ? error.slice(0, -1) : error;
-                //Remplaza la ultima coma por un " y "
-                error = error.replace(/,([^,]*)$/, ' y$1');
-                console.log("fecha inicio",FechaInicio);
-                console.log("fecha fin",FechaFin);
+  //               // Elimina la última coma si existe
+  //               error = error.endsWith(',') ? error.slice(0, -1) : error;
+  //               //Remplaza la ultima coma por un " y "
+  //               error = error.replace(/,([^,]*)$/, ' y$1');
+  //               console.log("Apps",Apps);
+  //               // console.log("fecha fin",FechaFin);
         
-             if (contError!=0) {
-                return res.status(400).send({
-                    error: error,
-                });
-             }
-            db.query(`CALL sp_CrearAviso(?,?,?,?,?,?)`,[IdApp,TextoInc,IdUsuario,FechaInicio,FechaFin,Titulo], (err, result) => {
-             if (err) {
-               return res.status(500).send({
-                 error: err,
-               });
-             }
+  //            if (contError!=0) {
+  //               return res.status(400).send({
+  //                   error: error,
+  //               });
+  //            }
+  //           db.query(`CALL sp_CrearAviso(?,?,?,?,?,?)`,[Id,TextoInc,IdUsuario,FechaInicio,FechaFin,Titulo,0], (err, result) => {
+  //            if (err) {
+  //              return res.status(500).send({
+  //                error: err,
+  //              });
+  //            }
        
-             if (result.length) {
-               const data = result[0];
-               return res.status(200).send({
-                 data,
-               });
-             } else {
-               return res.status(409).send({
-                 error: "¡Sin Información!",
-               });
-             }
-           });
-  },  
+  //            if (result.length) {
+  //              const data = result[0];
+  //              return res.status(200).send({
+  //                data,
+  //              });
+  //            } else {
+  //              return res.status(409).send({
+  //                error: "¡Sin Información!",
+  //              });
+  //            }
+  //          });
+  // },  
 
   getAdminAvisos:(req,res)=>{
     const {IdApp} = req.query;
