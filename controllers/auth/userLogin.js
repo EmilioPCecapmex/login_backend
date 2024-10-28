9504607c5e3ced66e32d1eb624b7f0147e21be38
PedroPardoG <prpardo@cecapmex.com>
2024-10-28 10:27:22 -0600

    configuracion para pruebas de refresh token

--- controllers/auth/userLogin.js
+++ controllers/auth/userLogin.js
@@ -132,7 +132,7 @@ module.exports = {
           };
           
           const token = jwt.sign(user, process.env.LOGIN_B_APP_JWT_SECRET_KEY, {
-            expiresIn: "45m",
+            expiresIn: "15m",
           });
           const response = {
             token: token,
