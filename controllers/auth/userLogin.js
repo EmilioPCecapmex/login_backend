cae120af100b3ed2e8ba258ca640e64bc6bf7a6b
PedroPardoG <112125266+PedroPardoG@users.noreply.github.com>
2025-08-28 15:40:38 -0600

    cambio

--- controllers/auth/userLogin.js
+++ controllers/auth/userLogin.js
@@ -68,7 +68,7 @@ module.exports = {
                 };
 
                 token = jwt.sign(user, process.env.LOGIN_B_APP_JWT_SECRET_KEY, {
-                  expiresIn: "45m",
+                  expiresIn: "10m",
                 });
 
                 const refreshToken = jwt.sign(
@@ -132,7 +132,7 @@ module.exports = {
           };
           
           const token = jwt.sign(user, process.env.LOGIN_B_APP_JWT_SECRET_KEY, {
-            expiresIn: "45m",
+            expiresIn: "10m",
           });
           const response = {
             token: token,
