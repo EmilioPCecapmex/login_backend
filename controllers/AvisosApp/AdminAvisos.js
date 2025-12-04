23637341b8bd985b43ca5ac0966842012b5b4aae
Gerart09 <gagarcia@cecapmex.com>
2025-12-04 17:14:22 -0600

    Si
    Si

--- controllers/AvisosApp/AdminAvisos.js
+++ controllers/AvisosApp/AdminAvisos.js
@@ -315,49 +315,6 @@ deleteAdminAvisos:(req,res)=>{
          });
 },
 
-getAdminAvisos:(req,res)=>{
-  const {IdApp} = req.query;
-
-  let contError=0;
-       let error = "Ingrese:";
-
-           if (!IdApp || (/^[\s]*$/.test(IdApp)))
-           {
-               error += " ch_IdApp";
-               contError++;
-           } 
-       
-           // Elimina la última coma si existe
-           error = error.endsWith(',') ? error.slice(0, -1) : error;
-           //Remplaza la ultima coma por un " y "
-           error = error.replace(/,([^,]*)$/, ' y$1');
-   
-        if (contError!=0) {
-           return res.status(400).send({
-               error: error,
-           });
-        }
-
-        db.query(`CALL sp_ListaAvisos(?)`,[IdApp], (err, result) => {
-           if (err) {
-             return res.status(500).send({
-               error: err,
-             });
-           }
-     
-           if (result.length) {
-             const data = result[0];
-             return res.status(200).send({
-               data,
-             });
-           } else {
-             return res.status(409).send({
-               error: "¡Sin Información!",
-             });
-           }
-         });
-},
-
 getAvisosVigentes:(req,res)=>{
   const {IdApp} = req.query;
   console.log("IdApp",IdApp);
--- controllers/Catalogos/Permisos.js
+++ controllers/Catalogos/Permisos.js
@@ -86,7 +86,7 @@ module.exports = {
                 });
             } else {
                 return res.status(500).send({
-                    msg: "No se eliminó el permiso.",
+                    msg: "No se eliminó el permiso.", 
                 });
             }
         });
